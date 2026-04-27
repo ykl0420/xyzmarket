31272e92c3919d837a2e7a2878bac6ac38ef4428
ykl <2841688659@qq.com>
2026-04-27 15:55:31 +0800

    feat: 新增业务异常类 BusinessException 与其处理函数

--- src/main/java/com/example/xyzmarket/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/xyzmarket/exception/GlobalExceptionHandler.java
@@ -4,13 +4,19 @@ import com.example.xyzmarket.common.Result;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public Result<String> handleException(Exception e) {
+    public Result<Void> handleException(Exception e) {
         e.printStackTrace();
         return Result.error(e.getMessage());
+    }
+
+    @ExceptionHandler(BusinessException.class)
+    public Result<Void> handleBusinessException(BusinessException e) {
+        return Result.error(e.getCode(), e.getMessage());
     }
 
 }
